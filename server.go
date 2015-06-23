a7fb42c7b969093894d82ab1973b7bb8bfb3dd1b
Daveloper87 <dai.williams@bbc.co.uk>
2015-06-24 00:26:35 +0100

    initial commit

---
+++ server.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+import "go-test/handlers/home"
+
+func main() {
+	
+	r:= gin.Default()
+	
+	r.GET("/", home.GetHome)
+	
+    r.GET("/user/:name", home.GetUser)
+	
+	r.Run(":9000")
+	
+}
